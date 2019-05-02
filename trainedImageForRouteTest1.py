b2d5936271b99d4e512dfca1a99f2b08efbbfcce
yasas1 <32437531+yasas1@users.noreply.github.com>
2019-05-02 10:34:51 +0530

    changed to trained image for route detection

--- trainedImageForRouteTest1.py
+++ trainedImageForRouteTest1.py
@@ -9,48 +9,46 @@ index_params =dict(algorithm = FLANN_INDEX_KDTREE, trees = 5 )
 
 flann = cv2.FlannBasedMatcher(index_params, {})
 
-trainImg=cv2.imread("car.png",0)
+trainImg=cv2.imread("image11.png",0)
 trainKP,trainDesc=detector.detectAndCompute(trainImg,None)
 
-cam=cv2.VideoCapture(0)
+#cam=cv2.VideoCapture(0)
 
-while True:
-    ret, QueryImgBGR=cam.read()
-    QueryImg=cv2.cvtColor(QueryImgBGR,cv2.COLOR_BGR2GRAY)
-    queryKP,queryDesc=detector.detectAndCompute(QueryImg,None)
-    matches=flann.knnMatch(queryDesc,trainDesc,k=2)
+#while True:
+    #ret, QueryImgBGR=cam.read()
+QueryImgBGR=cv2.imread("image1.png")
+QueryImg=cv2.cvtColor(QueryImgBGR,cv2.COLOR_BGR2GRAY)
+queryKP,queryDesc=detector.detectAndCompute(QueryImg,None)
+matches=flann.knnMatch(queryDesc,trainDesc,k=2)
 
-    goodMatch=[]
-    for m,n in matches:
-        if(m.distance<0.75*n.distance):
-            goodMatch.append(m)
+goodMatch=[]
+for m,n in matches:
+    if(m.distance<0.75*n.distance):
+        goodMatch.append(m)
 
-    MIN_MATCH_COUNT=30
-    if(len(goodMatch)>=MIN_MATCH_COUNT):
-        tp=[]
-        qp=[]
+MIN_MATCH_COUNT=30
+if(len(goodMatch)>=MIN_MATCH_COUNT):
+    tp=[]
+    qp=[]
 
-        for m in goodMatch:
-            tp.append(trainKP[m.trainIdx].pt)
-            qp.append(queryKP[m.queryIdx].pt)
+    for m in goodMatch:
+        tp.append(trainKP[m.trainIdx].pt)
+        qp.append(queryKP[m.queryIdx].pt)
 
-        tp,qp=np.float32((tp,qp))
+    tp,qp=np.float32((tp,qp))
 
-        H,status=cv2.findHomography(tp,qp,cv2.RANSAC,3.0)
+    H,status=cv2.findHomography(tp,qp,cv2.RANSAC,3.0)
 
-        h,w=trainImg.shape
-        trainBorder=np.float32([[[0,0],[0,h-1],[w-1,h-1],[w-1,0]]])
-        queryBorder=cv2.perspectiveTransform(trainBorder,H)
-        cv2.polylines(QueryImgBGR,[np.int32(queryBorder)],True,(0,255,0),5)
-    else:
-        print("Not Enough match found- %d/%d"%(len(goodMatch),MIN_MATCH_COUNT))
+    h,w=trainImg.shape
+    trainBorder=np.float32([[[0,0],[0,h-1],[w-1,h-1],[w-1,0]]])
+    queryBorder=cv2.perspectiveTransform(trainBorder,H)
+    cv2.polylines(QueryImgBGR,[np.int32(queryBorder)],True,(0,255,0),5)
+else:
+    print("Not Enough match found- %d/%d"%(len(goodMatch),MIN_MATCH_COUNT))
 
-    cv2.imshow('result',QueryImgBGR)
-    if cv2.waitKey(10)==ord('q'):
-        break
-cam.release()
-cv2.destroyAllWindows()
+cv2.imshow('result',QueryImgBGR)
+#if cv2.waitKey(10)==ord('q'):
+   
+#cam.release()
+#cv2.destroyAllWindows()
 
-
-
-        
