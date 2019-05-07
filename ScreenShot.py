64597491dda6d00c75bdfea5e4ec8d33edd48784
yasas1 <32437531+yasas1@users.noreply.github.com>
2019-05-07 12:20:23 +0530

    updated

--- ScreenShot.py
+++ ScreenShot.py
@@ -5,6 +5,7 @@ import webbrowser
 
 webbrowser.open('https://www.google.com/maps/dir/Lumbini+Bus+Stop,+Sri+Sambuddhathva+Jayanthi+Mawatha,+Colombo/University+of+Colombo,+College+House,+Kumaratunga+Munidasa+Mawatha,+Colombo/@6.8909493,79.8561933,15z/data=!3m1!4b1!4m13!4m12!1m5!1m1!1s0x3ae25bce93e5496f:0x622bcc171fe22cd4!2m2!1d79.8677945!2d6.8844459!1m5!1m1!1s0x3ae2596309dfdd3f:0x45a4b0e7834ac0d4!2m2!1d79.8587938!2d6.9000149')
 
+
 import pyautogui
 import time
 
