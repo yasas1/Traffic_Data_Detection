19aa5beb403da3a4f42f8cfbda291b0cd452544d
yasas1 <32437531+yasas1@users.noreply.github.com>
2019-05-07 11:42:38 +0530

    updated

--- test3/gratScaleToBinary.py
+++
@@ -1,6 +0,0 @@
-from PIL import Image
-col = Image.open("test7crop.png") #read image
-gray = col.convert('L')  #conversion to gray scale
-bw = gray.point(lambda x: 0 if x<205 else 255, '1')  #binarization
-#bw.save("trainedBinary5.png") #save it
-bw.save("testBinary8.png")
