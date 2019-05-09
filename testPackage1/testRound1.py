f382e8971707dccf14ed60414af8b34804595396
yasas1 <32437531+yasas1@users.noreply.github.com>
2019-05-09 22:42:08 +0530

    updated

--- testPackage1/testRound1.py
+++ testPackage1/testRound1.py
@@ -7,9 +7,11 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 retval, QueryImgBGR = cv2.threshold(gray, 185, 255, cv2.THRESH_BINARY)
 
+#Route detecting
+
 MIN_MATCH_COUNT = 30
 
-detector = cv2.xfeatures2d.SIFT_create() #cv2.SIFT()
+detector = cv2.xfeatures2d.SIFT_create()
 FLANN_INDEX_KDTREE = 0
 index_params =dict(algorithm = FLANN_INDEX_KDTREE, trees = 5 )
 
@@ -42,28 +44,22 @@ if(len(goodMatch)>=MIN_MATCH_COUNT):
     H,status=cv2.findHomography(tp,qp,cv2.RANSAC,3.0)
 
     h,w=trainImg.shape
-    print(h)
-    print(w)
     
     trainBorder=np.float32([[[0,0],[0,h-1],[w-1,h-1],[w-1,0]]])
-    print(trainBorder)
+
     queryBorder=cv2.perspectiveTransform(trainBorder,H)
-    print(np.int32(queryBorder))
 
     y = min(np.int32(queryBorder[0][3][1]),np.int32(queryBorder[0][0][1]))
     x = min(np.int32(queryBorder[0][0][0]),np.int32(queryBorder[0][1][0]))
-
-    print(y)
-    print(x)
-    
-    cv2.imshow('Original',image)
     
     routeDetected = image[y:y+h, x:x+w]
-    cv2.imshow('Detected', routeDetected)
-
+    
+    cv2.imshow('Original',image)
+    #cv2.imshow('Detected', routeDetected)
     #cv2.polylines(QueryImgBGR,[np.int32(queryBorder)],True,(0,255,0),5)
-
-    hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    
+    #color detecting
+    hsv=cv2.cvtColor(routeDetected,cv2.COLOR_BGR2HSV)
 
     orange_lower=np.array([10,70,70],np.uint8)
     orange_upper=np.array([20,255,255],np.uint8)
@@ -82,15 +78,11 @@ if(len(goodMatch)>=MIN_MATCH_COUNT):
         print(area)
         if(area>100):
             x,y,w,h = cv2.boundingRect(contour)
-            img = cv2.rectangle(img,(x,y),(x+w,y+h),(0, 128, 255),2)
-            cv2.putText(img,"Orange",(x,y),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0, 128, 255))
+            colorteDetected = cv2.rectangle(routeDetected,(x,y),(x+w,y+h),(0, 128, 255),2)
+            cv2.putText(colorteDetected,"Orange",(x,y),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0, 128, 255))
 
+    cv2.imshow("Color Detecting",colorteDetected)
 
-
-    #cv2.imshow("Redcolour",red)
-    cv2.imshow("Color Tracking",img)
-
-    
 else:
     print("Not Enough match found- %d/%d"%(len(goodMatch),MIN_MATCH_COUNT))
 
