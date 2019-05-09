bec3175b259112a565eed8ad91ac4570babf3c76
yasas1 <32437531+yasas1@users.noreply.github.com>
2019-05-09 21:57:58 +0530

    updated

--- testPackage1/ColorDetectionOrange.py
+++ testPackage1/ColorDetectionOrange.py
@@ -5,26 +5,26 @@ img = cv2.imread("CropDirTest.png")
 
 hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
 
-red_lower=np.array([10,70,70],np.uint8)
-red_upper=np.array([20,255,255],np.uint8)
+orange_lower=np.array([10,70,70],np.uint8)
+orange_upper=np.array([20,255,255],np.uint8)
 
-red=cv2.inRange(hsv, red_lower, red_upper)
+orange=cv2.inRange(hsv, orange_lower, orange_upper)
 
 kernal = np.ones((5 ,5), "uint8")
 
-red=cv2.dilate(red, kernal)
-res=cv2.bitwise_and(img, img, mask = red)
+orange=cv2.dilate(orange, kernal)
+#res=cv2.bitwise_and(img, img, mask = red)
 
 #Tracking the Red Color
-(_,contours,hierarchy)=cv2.findContours(red,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+(_,contours,hierarchy)=cv2.findContours(orange,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
 for pic, contour in enumerate(contours):
     area = cv2.contourArea(contour)
     print(area)
     if(area>100):
         x,y,w,h = cv2.boundingRect(contour)
-        img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
-        cv2.putText(img,"RED",(x,y),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255))
+        img = cv2.rectangle(img,(x,y),(x+w,y+h),(0, 128, 255),2)
+        cv2.putText(img,"Orange",(x,y),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0, 128, 255))
 
 
 
