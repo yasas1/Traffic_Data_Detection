ba665efb47ffe16a1717dd8d3c0dbd2bd4b6a918
yasas1 <32437531+yasas1@users.noreply.github.com>
2019-05-09 22:31:43 +0530

    updated

--- testPackage1/testRound1.py
+++ testPackage1/testRound1.py
@@ -62,6 +62,35 @@ if(len(goodMatch)>=MIN_MATCH_COUNT):
     cv2.imshow('Detected', routeDetected)
 
     #cv2.polylines(QueryImgBGR,[np.int32(queryBorder)],True,(0,255,0),5)
+
+    hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+
+    orange_lower=np.array([10,70,70],np.uint8)
+    orange_upper=np.array([20,255,255],np.uint8)
+
+    orange=cv2.inRange(hsv, orange_lower, orange_upper)
+
+    kernal = np.ones((5 ,5), "uint8")
+
+    orange=cv2.dilate(orange, kernal)
+    
+    #Tracking the Red Color
+    (_,contours,hierarchy)=cv2.findContours(orange,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+    for pic, contour in enumerate(contours):
+        area = cv2.contourArea(contour)
+        print(area)
+        if(area>100):
+            x,y,w,h = cv2.boundingRect(contour)
+            img = cv2.rectangle(img,(x,y),(x+w,y+h),(0, 128, 255),2)
+            cv2.putText(img,"Orange",(x,y),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0, 128, 255))
+
+
+
+    #cv2.imshow("Redcolour",red)
+    cv2.imshow("Color Tracking",img)
+
+    
 else:
     print("Not Enough match found- %d/%d"%(len(goodMatch),MIN_MATCH_COUNT))
 
