e9ca0e08e0f30b22f371140bfa029d419d3ee1fb
yasas1 <32437531+yasas1@users.noreply.github.com>
2019-06-13 22:49:44 +0530

    updated

--- testPackage2/Test2.py
+++ testPackage2/Test2.py
@@ -1,4 +1,4 @@
-
+import cv2   
 import numpy as np
 import pyautogui
 import time
@@ -11,13 +11,20 @@ import urllib
 
 
 refreshrate=5
+
 driver = webdriver.Chrome(r"C:\Users\Dell\Desktop\chromedriver.exe")
 driver.maximize_window()
-#driver.set_window_size(1500, 1000)
 driver.get("https://www.google.com/maps/@6.8874059,79.8676187,14.78z/data=!5m1!1e1")
+time.sleep(4)
 
-while True:
+x=1
+
+while x<4:
+    
+    scr = pyautogui.screenshot()
+    scr.save(r"D:\Traffic Data\Traffic_Data_Detection\Image\newIm"+str(x)+".png")
+    x+=1
     time.sleep(refreshrate)
     driver.refresh()
 
-
+driver.close()
